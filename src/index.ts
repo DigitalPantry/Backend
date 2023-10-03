d729cc4a547f6d07386097025deacd31d3302c2c
Logan Ellsworth <97066574+LoganEllsworth@users.noreply.github.com>
2023-10-03 13:48:57 -0700

    WIP Changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import router from './router';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8081;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/api', router);
 
 app.get('/healthcheck', (req: Request, res: Response) => {
   res.statusCode = 200;
