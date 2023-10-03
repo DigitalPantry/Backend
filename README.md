249e3bfb4fb523470f2ebc40d39b024c94e37c19
Austin Christiansen <amchristiansen7@gmail.com>
2023-10-03 10:57:43 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Digital Pantry Backend
+
+### Authors
+Austin Christiansen
+
+Jackson Rhea
+
+Logan Ellsworth
+
+### Development
+To run the server locally, run `npm run dev` in the root directory. 
+This will start the server on port 8080 (or the port specified in the .env file).
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 8081;
+
+app.get('/healthcheck', (req: Request, res: Response) => {
+  res.statusCode = 200;
+  res.send({ status: 'success' });
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
