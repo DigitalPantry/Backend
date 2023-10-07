062f991f870f6691139e3a35ae637c097a645a02
Logan Ellsworth <97066574+LoganEllsworth@users.noreply.github.com>
2023-10-07 11:09:56 -0700

    removed /r

--- db/migrate.sh
+++ db/migrate.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd migrations
-goose postgres postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/$POSTGRES_DB?sslmode=prefer up
+cd migrations; goose postgres postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/$POSTGRES_DB?sslmode=prefer up
